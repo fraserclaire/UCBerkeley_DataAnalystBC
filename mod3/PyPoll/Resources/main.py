4f763f7dbec347260928aa72440ebe2e97ec8ef0
Claire Fraser <clairefraser7@gmail.com>
2023-11-05 12:36:51 -0800

    update mod3

---
+++ mod3/PyPoll/Resources/main.py
@@ -0,0 +1,17 @@
+#PyPoll
+
+#need to help with vote counting
+
+#import the dataset
+"election_data.csv"
+
+#Calculate the following values:
+#1. The total number of votes cast
+
+#2. A complete list of candidates who received votes
+
+#3. The percentage of votes each candidate won
+
+#4. The total number of votes each candidate won
+
+#5. The winner of the election based on popular vote
